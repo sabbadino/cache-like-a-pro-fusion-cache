0015ed6e7b3c941db54f7651e9970a067ac2b254
Enrico Sabbadin <enrico.sabbadin@msc.com>
2024-06-21 17:56:59 +0200

    swagger ui always available

--- fusionCacheApi/Program.cs
+++ fusionCacheApi/Program.cs
@@ -63,12 +63,10 @@ namespace fusionCacheApi
 
             var app = builder.Build();
 
-            // Configure the HTTP request pipeline.
-            if (app.Environment.IsDevelopment())
-            {
-                app.UseSwagger();
-                app.UseSwaggerUI();
-            }
+           
+            app.UseSwagger();
+            app.UseSwaggerUI();
+          
 
             app.UseHttpsRedirection();
 
